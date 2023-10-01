8fc256e16408ce9de3750972d38936e1a7e47c44
Bui Tuan Linh <linh97abc@gmail.com>
2023-10-01 22:59:39 +0700

    scripts: fix dow sw

--- scripts/dow_sw.sh
+++ scripts/dow_sw.sh
@@ -1,2 +1,3 @@
+nios2-configure-sof boot.sof
 nios2-flash-programmer --epcs --base=0x800b000 --override=nios2-flash-override.txt  sw.cfg.flash
 nios2-flash-programmer --epcs --base=0x800b000 --override=nios2-flash-override.txt  sw.flash
